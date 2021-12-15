d1d91b060dae0b378e328c8af9187062ac4a262b
mozart409 <38767929+Mozart409@users.noreply.github.com>
2021-12-15 18:32:41 +0100

    build: :rocket: added production deployment

---
+++ backend/config/env/production/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '68caaf033fed944f4d3b4cd26cffdc63'),
+    },
+  },
+});
