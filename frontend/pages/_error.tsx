9922b92cbae33e224527ccc1d7efa7320a5e3973
mozart409 <38767929+Mozart409@users.noreply.github.com>
2021-12-16 15:15:30 +0100

    feat: :sparkles: update error page and bootstrap config

--- frontend/pages/_error.tsx
+++ frontend/pages/_error.tsx
@@ -16,18 +16,13 @@ function Error({ statusCode }) {
           <main className="flex-grow flex flex-col justify-center max-w-7xl w-full mx-auto px-4 sm:px-6 lg:px-8">
             <div className="flex-shrink-0 flex justify-center">
               <a href="/" className="inline-flex">
-                <span className="sr-only">Workflow</span>
-                <img
-                  className="h-12 w-auto"
-                  src="https://tailwindui.com/img/logos/workflow-mark.svg?color=indigo&shade=600"
-                  alt=""
-                />
+                <span className="">Home</span>
               </a>
             </div>
             <div className="py-16">
               <div className="text-center">
-                <p className="text-sm font-semibold text-indigo-600 uppercase tracking-wide">
-                  404 error
+                <p className="text-sm font-semibold text-primary-600 uppercase tracking-wide">
+                  {statusCode} error
                 </p>
                 <h1 className="mt-2 text-4xl font-extrabold text-gray-900 tracking-tight sm:text-5xl">
                   Page not found.
@@ -37,8 +32,8 @@ function Error({ statusCode }) {
                 </p>
                 <div className="mt-6">
                   <a
-                    href="#"
-                    className="text-base font-medium text-indigo-600 hover:text-indigo-500"
+                    href="/"
+                    className="text-base font-medium text-primary-600 hover:text-primary-500"
                   >
                     Go back home<span aria-hidden="true"> &rarr;</span>
                   </a>
@@ -46,36 +41,6 @@ function Error({ statusCode }) {
               </div>
             </div>
           </main>
-          <footer className="flex-shrink-0 max-w-7xl w-full mx-auto px-4 sm:px-6 lg:px-8">
-            <nav className="flex justify-center space-x-4">
-              <a
-                href="#"
-                className="text-sm font-medium text-gray-500 hover:text-gray-600"
-              >
-                Contact Support
-              </a>
-              <span
-                className="inline-block border-l border-gray-300"
-                aria-hidden="true"
-              />
-              <a
-                href="#"
-                className="text-sm font-medium text-gray-500 hover:text-gray-600"
-              >
-                Status
-              </a>
-              <span
-                className="inline-block border-l border-gray-300"
-                aria-hidden="true"
-              />
-              <a
-                href="#"
-                className="text-sm font-medium text-gray-500 hover:text-gray-600"
-              >
-                Twitter
-              </a>
-            </nav>
-          </footer>
         </div>
       </div>
     </div>
