7a6ca439e529b57bb4b4187a12ad21501abe95f6
mozart409 <38767929+Mozart409@users.noreply.github.com>
2021-12-18 11:09:17 +0100

    feat: :lipstick: changed navbar styling

--- frontend/components/elements/navbar.tsx
+++ frontend/components/elements/navbar.tsx
@@ -85,56 +85,65 @@ const Navbar = ({ navbar, pageContext }: INavbar) => {
 
             <ul className="hidden flex-row gap-4 items-baseline ml-10 list-none md:flex">
               {navbar.links.map((menuLink) => (
-                <Menu
-                  key={menuLink.id}
-                  as="div"
-                  className="relative inline-block text-left"
-                >
-                  <div>
-                    <Menu.Button className="inline-flex justify-center w-full rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-white text-sm font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-100 focus:ring-primary-500">
+                <div key={menuLink.id}>
+                  {menuLink.links[0] ? (
+                    <Menu as="div" className="relative inline-block text-left">
+                      <div>
+                        <div>
+                          {console.log(menuLink)}
+                          <Menu.Button className="inline-flex justify-center w-full rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-white text-sm font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-100 focus:ring-primary-500">
+                            <Link href={menuLink.url} passHref>
+                              <a title={menuLink.title}>{menuLink.text}</a>
+                            </Link>
+                            <ChevronDownIcon
+                              className="-mr-1 ml-2 h-5 w-5"
+                              aria-hidden="true"
+                            />
+                          </Menu.Button>
+                        </div>
+
+                        <Transition
+                          as={Fragment}
+                          enter="transition ease-out duration-100"
+                          enterFrom="transform opacity-0 scale-95"
+                          enterTo="transform opacity-100 scale-100"
+                          leave="transition ease-in duration-75"
+                          leaveFrom="transform opacity-100 scale-100"
+                          leaveTo="transform opacity-0 scale-95"
+                        >
+                          <Menu.Items className="origin-top-right absolute right-0 mt-2 w-56 rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5 focus:outline-none">
+                            <div className="py-1">
+                              {menuLink.links.map((item) => (
+                                <Menu.Item key={item.id}>
+                                  {({ active }) => (
+                                    <Link href={item.url}>
+                                      <a
+                                        className={classNames(
+                                          active
+                                            ? 'bg-gray-100 text-gray-900'
+                                            : 'text-gray-700',
+                                          'block px-4 py-2 text-sm hover:bg-gray-100'
+                                        )}
+                                      >
+                                        {item.text}
+                                      </a>
+                                    </Link>
+                                  )}
+                                </Menu.Item>
+                              ))}
+                            </div>
+                          </Menu.Items>
+                        </Transition>
+                      </div>
+                    </Menu>
+                  ) : (
+                    <div>
                       <Link href={menuLink.url} passHref>
                         <a title={menuLink.title}>{menuLink.text}</a>
                       </Link>
-                      <ChevronDownIcon
-                        className="-mr-1 ml-2 h-5 w-5"
-                        aria-hidden="true"
-                      />
-                    </Menu.Button>
-                  </div>
-
-                  <Transition
-                    as={Fragment}
-                    enter="transition ease-out duration-100"
-                    enterFrom="transform opacity-0 scale-95"
-                    enterTo="transform opacity-100 scale-100"
-                    leave="transition ease-in duration-75"
-                    leaveFrom="transform opacity-100 scale-100"
-                    leaveTo="transform opacity-0 scale-95"
-                  >
-                    <Menu.Items className="origin-top-right absolute right-0 mt-2 w-56 rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5 focus:outline-none">
-                      <div className="py-1">
-                        {menuLink.links.map((item) => (
-                          <Menu.Item key={item.id}>
-                            {({ active }) => (
-                              <Link href={item.url}>
-                                <a
-                                  className={classNames(
-                                    active
-                                      ? 'bg-gray-100 text-gray-900'
-                                      : 'text-gray-700',
-                                    'block px-4 py-2 text-sm hover:bg-gray-100'
-                                  )}
-                                >
-                                  {item.text}
-                                </a>
-                              </Link>
-                            )}
-                          </Menu.Item>
-                        ))}
-                      </div>
-                    </Menu.Items>
-                  </Transition>
-                </Menu>
+                    </div>
+                  )}
+                </div>
               ))}
             </ul>
           </div>
