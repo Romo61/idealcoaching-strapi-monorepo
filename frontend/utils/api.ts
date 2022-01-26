bc9924709745673176498a71842e3820d5d9d0b8
Amadeus <38767929+Mozart409@users.noreply.github.com>
2022-01-26 18:56:39 +0100

    fix: :mute: removed log

--- frontend/utils/api.ts
+++ frontend/utils/api.ts
@@ -1,5 +1,5 @@
 export function getStrapiURL(path: string) {
-  console.log(process.env.NEXT_PUBLIC_STRAPI_API_URL)
+  //  console.log(process.env.NEXT_PUBLIC_STRAPI_API_URL)
   return `${
     process.env.NEXT_PUBLIC_STRAPI_API_URL || 'http://localhost:1337'
   }${path}`
