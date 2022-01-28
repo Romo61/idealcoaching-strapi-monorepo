7cf6aef9809a29a07d0c05d475e7d999846a20ca
Isabell <55280783+CheetahGrete@users.noreply.github.com>
2022-01-28 16:23:26 +0100

    Update _document.tsx

--- frontend/pages/_document.tsx
+++ frontend/pages/_document.tsx
@@ -6,14 +6,14 @@ export default class MyDocument extends Document {
     return (
       <Html lang="de">
         <Head>
-          {/* <script
+           <script
             id="usercentrics-cmp"
-            data-settings-id="XXXXXXX"
+            data-settings-id="OE_qAIEQ8"
             src="https://app.usercentrics.eu/browser-ui/latest/bundle.js"
             defer
-          ></script> */}
+          ></script> 
 
-          {/* <script
+           <script
             data-usercentrics="Google Tag Manager"
             type="text/plain"
             async
@@ -24,10 +24,10 @@ export default class MyDocument extends Document {
               new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
               j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
               'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-              })(window,document,'script','dataLayer', 'GTM-XXXX');
+              })(window,document,'script','dataLayer', 'GTM-TKNQ7FV');
               `,
             }}
-          /> */}
+          /> 
           <link rel="preload" href="/fonts/inter-var-latin.woff2"></link>
 
           <link rel="preload" href="/fonts/fira/FiraSans-Bold.woff" />
@@ -42,12 +42,12 @@ export default class MyDocument extends Document {
 
         <body>
           <noscript>
-            {/* <iframe
-              src="https://www.googletagmanager.com/ns.html?id=GTM-XXXX"
+            <iframe
+              src="https://www.googletagmanager.com/ns.html?id=GTM-TKNQ7FV"
               height="0"
               width="0"
               style={{ display: 'none', visibility: 'hidden' }}
-            /> */}
+            />
           </noscript>
           <Main />
           <NextScript />
