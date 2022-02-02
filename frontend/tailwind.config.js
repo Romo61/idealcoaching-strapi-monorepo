18f4d436cbbc582b7d26f9860d62c68deba1657a
Isabell <55280783+CheetahGrete@users.noreply.github.com>
2022-02-02 10:42:08 +0100

    colour change 2

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -9,7 +9,7 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        primary: colors.orange,
+        primary: colors.slate,
         cyan: colors.cyan,
         yellow: colors.amber,
         orange: colors.orange,
