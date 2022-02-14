35c10e588259900a5dac33c5ce518e32bf3425da
Mozart409 <maderamadeus@gmail.com>
2022-02-14 12:44:54 +0100

    fix: :mag: added more redirects

--- frontend/components/sections/gradient-hero.tsx
+++ frontend/components/sections/gradient-hero.tsx
@@ -1,10 +1,10 @@
-import MarkdownRender from "utils/MarkdownRender"
-import ButtonLink from "../elements/button-link"
-import CustomImage from "../elements/image"
-import { getButtonAppearance } from "utils/button"
-import React, { ReactElement, useState } from "react"
-import Link from "next/link"
-import CustomLink from "./custom-link"
+import MarkdownRender from 'utils/MarkdownRender'
+import ButtonLink from '../elements/button-link'
+import CustomImage from '../elements/image'
+import { getButtonAppearance } from 'utils/button'
+import React, { ReactElement, useState } from 'react'
+import Link from 'next/link'
+import CustomLink from './custom-link'
 
 export interface GradientHero {
   data: {
@@ -14,7 +14,7 @@ export interface GradientHero {
     blackText: string
     coloredText: string
     content: string
-    title_color: "black" | "orange" | "green" | "yellow" | "blue" | "red"
+    title_color: 'black' | 'orange' | 'green' | 'yellow' | 'blue' | 'red'
     seminarcard: Seminarcard[]
   }
 }
@@ -73,8 +73,8 @@ const SingleCard = ({
       <Link href={url}>
         <a
           // Change target and rel attributes is newTab is turned on
-          target={newTab ? "_blank" : "_self"}
-          rel={newTab ? "noopener noreferrer" : ""}
+          target={newTab ? '_blank' : '_self'}
+          rel={newTab ? 'noopener noreferrer' : ''}
           title={tilte}
         >
           <div className="relative flex-shrink-0">
