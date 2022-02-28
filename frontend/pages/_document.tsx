a6c916c0b259c496d57faba029d6c94d03c636ba
Isabell <55280783+CheetahGrete@users.noreply.github.com>
2022-02-28 12:24:50 +0100

    fix: :zap: improved usercentric performance

--- frontend/pages/_document.tsx
+++ frontend/pages/_document.tsx
@@ -6,12 +6,8 @@ export default class MyDocument extends Document {
     return (
       <Html lang="de">
         <Head>
-          <script
-            id="usercentrics-cmp"
-            data-settings-id="OE_qAIEQ8"
-            src="https://app.usercentrics.eu/browser-ui/latest/bundle.js"
-            defer
-          ></script>
+        
+          <script id="usercentrics-cmp" src="https://app.usercentrics.eu/browser-ui/latest/loader.js" data-settings-id="OE_qAIEQ8" async></script> 
 
           <script
             data-usercentrics="Google Tag Manager"
