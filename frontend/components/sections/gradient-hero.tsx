cd19b3cbdc15a005435c90135ca69bede12450f0
Mozart409 <maderamadeus@gmail.com>
2022-03-01 16:25:30 +0100

    style: :lipstick: added prettier

--- frontend/components/sections/gradient-hero.tsx
+++ frontend/components/sections/gradient-hero.tsx
@@ -87,7 +87,7 @@ const SingleCard = ({
             <div className="absolute bottom-0 left-0 w-full">
               <div className="mx-auto flex flex-1 flex-col justify-between bg-white py-2 px-6">
                 <div className="flex-1">
-                  <p className="text-md bg-gradient-to-r from-green-500 to-blue-500 bg-clip-text font-bold text-transparent">
+                  <p className="text-md bg-gradient-to-r from-green-500 to-blue-500 bg-clip-text text-left font-bold text-transparent">
                     {category}
                   </p>
                   <div className="mt-2 hidden lg:block">
--- frontend/prettier.config.js
+++ frontend/prettier.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   plugins: [require('prettier-plugin-tailwindcss')],
   printWidth: 80,
-  singleQuote: true,
+  singleQuote: false,
   trailingComma: 'es5',
   semi: false,
   tabWidth: 2,
