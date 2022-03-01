b1559517622277733e8f1363482745c7e173e138
Mozart409 <maderamadeus@gmail.com>
2022-03-01 16:06:59 +0100

    feat: :lipstick: updated styling

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -9,7 +9,7 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        primary: colors.amber,
+        primary: colors.emerald,
         cyan: colors.cyan,
         yellow: colors.amber,
         orange: colors.orange,
