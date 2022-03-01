df975bc7b986347de732fad8ddb0f7ca745cbe72
Romo61 <71834071+Romo61@users.noreply.github.com>
2022-03-01 16:27:06 +0100

    dddasd (#6)
    * text left
    
    * style: :lipstick: double quotes
    
    * style: :lipstick: format
    
    Co-authored-by: Mozart409 <maderamadeus@gmail.com>

--- frontend/prettier.config.js
+++ frontend/prettier.config.js
@@ -2,7 +2,7 @@ module.exports = {
   plugins: [require('prettier-plugin-tailwindcss')],
   printWidth: 80,
   singleQuote: false,
-  trailingComma: 'es5',
+  trailingComma: "es5",
   semi: false,
   tabWidth: 2,
 }
